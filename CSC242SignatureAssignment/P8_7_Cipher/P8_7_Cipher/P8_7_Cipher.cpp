894f02f0751acdeeb4b3897ee1c7035dfb72beee
Jacob Leigers <leigersjacob5@gmail.com>
2026-08-15 09:45:50 -0700

    Update P8_7_Cipher.cpp

--- CSC242SignatureAssignment/P8_7_Cipher/P8_7_Cipher/P8_7_Cipher.cpp
+++ CSC242SignatureAssignment/P8_7_Cipher/P8_7_Cipher/P8_7_Cipher.cpp
@@ -75,6 +75,14 @@ string createCipherAlphabet(const string& keyword)
 // ----------------------------------------------------
 char encryptCharacter(char ch, const string& cipher)
 {
+    if (ch >= 'a' && ch <= 'z')
+    { 
+        return cipher[ch - 'a'];
+    }
+    if (ch >= 'A' && <= 'Z')
+    {
+        return cipher[ch - 'A'];
+    }
     return ch;
 }
 
